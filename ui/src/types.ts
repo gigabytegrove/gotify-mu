54c1f85353fc7315b92dbd1a1bfd90b320a12a50
Brad Trammell <bradjtrammell@gmail.com>
2026-09-24 17:49:02 -0400

    fix(chat): standardize UI on allowMemberPost

--- ui/src/types.ts
+++ ui/src/types.ts
@@ -3,7 +3,7 @@ export interface IApplication {
     token: string;
     ownerId?: number;
     autoAssign?: boolean;
-    membersCanPost?: boolean;
+    allowMemberPost?: boolean;
     receiveNotifications?: boolean;
     name: string;
     sortKey: string;
