c967cf2b2c1c1ef1005bfd75b24fdc82295b211d
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 19:16:20 -0400

    authz: clean Channel preferences when users are deleted

--- database/user.go
+++ database/user.go
@@ -116,6 +116,9 @@ func (d *GormDatabase) DeleteUserByID(id uint) error {
 	if err := d.DB.Where("user_id = ?", id).Delete(&model.QuietHoursPolicy{}).Error; err != nil {
 		return err
 	}
+	if err := d.DB.Where("user_id = ?", id).Delete(&model.ApplicationNotificationPreference{}).Error; err != nil {
+		return err
+	}
 	if err := d.DeleteUserGroupMembershipsForUser(id); err != nil {
 		return err
 	}
