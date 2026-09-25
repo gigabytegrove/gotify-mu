cda02a32a92d7525732b0e04640b9498bc267047
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 19:02:17 -0400

    reliability: migrate scheduled run history

--- database/database.go
+++ database/database.go
@@ -111,6 +111,7 @@ func New(dialect, connection, defaultUser, defaultPass string, strength int, cre
 		new(model.EscalationState),
 		new(model.MessageAcknowledgement),
 		new(model.AutomationLease),
+		new(model.ScheduledRun),
 	); err != nil {
 		return nil, err
 	}
