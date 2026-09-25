600e40f6840943cd7d52d853981e22ba2035408b
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 19:54:28 -0400

    db: clean deferred notifications when Channels are deleted

--- database/application.go
+++ database/application.go
@@ -103,6 +103,7 @@ func (d *GormDatabase) DeleteApplicationByID(id uint) error {
 		if err := tx.Where("application_id = ?", id).Delete(&model.SyslogReceiver{}).Error; err != nil { return err }
 		if err := tx.Where("application_id = ?", id).Delete(&model.ScheduledNotification{}).Error; err != nil { return err }
 		if err := tx.Where("application_id = ?", id).Delete(&model.DigestItem{}).Error; err != nil { return err }
+		if err := tx.Where("application_id = ?", id).Delete(&model.DeferredNotification{}).Error; err != nil { return err }
 		if err := tx.Where("application_id = ?", id).Delete(&model.ApplicationMembership{}).Error; err != nil { return err }
 		if err := tx.Where("application_id = ?", id).Delete(&model.ApplicationGroupAssignment{}).Error; err != nil { return err }
 		return tx.Where("id = ?", id).Delete(&model.Application{}).Error
