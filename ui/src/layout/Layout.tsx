582f5ef18009b4f06a3e1b8e4bb7f0f0b1b244da
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 15:05:11 -0400

    feat: add system administration route

--- ui/src/layout/Layout.tsx
+++ ui/src/layout/Layout.tsx
@@ -31,6 +31,7 @@ import Groups from '../group/Groups';
 import Audit from '../audit/Audit';
 import Integrations from '../integration/Integrations';
 import Automation from '../automation/Automation';
+import SystemAdministration from '../admin/SystemAdministration';
 import {observer} from 'mobx-react-lite';
 import {ConnectionErrorBanner} from '../common/ConnectionErrorBanner';
 import {useStores} from '../stores';
@@ -153,6 +154,10 @@ const Layout = observer(() => {
                                     <Route
                                         path="/automation"
                                         element={authed(elevated(<Automation />))}
+                                    />
+                                    <Route
+                                        path="/system"
+                                        element={authed(elevated(<SystemAdministration />))}
                                     />
                                     <Route
                                         path="/settings"
