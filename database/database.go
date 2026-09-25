84c19745cd377cd733782f4780d4bf3ae95784c6
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 18:29:34 -0400

    db: migrate webhook delivery history

--- database/database.go
+++ database/database.go
@@ -102,6 +102,7 @@ func New(dialect, connection, defaultUser, defaultPass string, strength int, cre
 		new(model.UserGroup),
 		new(model.UserGroupMembership),
 		new(model.WebhookRoute),
+		new(model.WebhookDelivery),
 		new(model.MQTTIntegration),
 		new(model.HomeAssistantIntegration),
 		new(model.ScheduledNotification),
