d909c6bc5dfff61bb7bda4e758c2659581568470
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 19:24:32 -0400

    security: remove MFA state when users are deleted

--- database/user.go
+++ database/user.go
@@ -122,6 +122,9 @@ func (d *GormDatabase) DeleteUserByID(id uint) error {
 	if err := d.DB.Where("user_id = ?", id).Delete(&model.ServiceCredential{}).Error; err != nil {
 		return err
 	}
+	if err := d.DB.Where("user_id = ?", id).Delete(&model.UserMFA{}).Error; err != nil {
+		return err
+	}
 	if err := d.DeleteUserGroupMembershipsForUser(id); err != nil {
 		return err
 	}
