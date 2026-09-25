c3f04db70b71fcafef2551060db381cf606c9b9c
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 14:59:42 -0400

    fix: remove MFA enrollment with deleted users

--- database/user.go
+++ database/user.go
@@ -116,6 +116,9 @@ func (d *GormDatabase) DeleteUserByID(id uint) error {
 	if err := d.DB.Where("user_id = ?", id).Delete(&model.QuietHoursPolicy{}).Error; err != nil {
 		return err
 	}
+	if err := d.DB.Where("user_id = ?", id).Delete(&model.UserMFA{}).Error; err != nil {
+		return err
+	}
 	if err := d.DB.Where("user_id = ?", id).Delete(&model.DeferredNotification{}).Error; err != nil {
 		return err
 	}
