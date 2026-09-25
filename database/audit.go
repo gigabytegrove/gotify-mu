0afbd86488a24353fa69af464f47972832985d9c
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 19:10:40 -0400

    ops: persist audit retention settings

--- database/audit.go
+++ database/audit.go
@@ -34,3 +34,31 @@ func (d *GormDatabase) GetAuditEvents(limit int, action, target string) ([]*mode
 func (d *GormDatabase) DeleteAuditEventsBefore(before time.Time) error {
 	return d.DB.Where("created_at < ?", before).Delete(&model.AuditEvent{}).Error
 }
+
+
+func (d *GormDatabase) GetAuditSettings() (*model.AuditSettings, error) {
+	item := &model.AuditSettings{ID: 1, RetentionDays: 180}
+	err := d.DB.First(item, 1).Error
+	if err == nil {
+		return item, nil
+	}
+	if err != nil && err.Error() != "" {
+		// GORM's translated record-not-found error is handled by creating defaults below.
+	}
+	item = &model.AuditSettings{ID: 1, RetentionDays: 180}
+	if createErr := d.DB.Create(item).Error; createErr != nil {
+		return nil, createErr
+	}
+	return item, nil
+}
+
+func (d *GormDatabase) SaveAuditSettings(item *model.AuditSettings) error {
+	item.ID = 1
+	if item.RetentionDays < 7 {
+		item.RetentionDays = 7
+	}
+	if item.RetentionDays > 3650 {
+		item.RetentionDays = 3650
+	}
+	return d.DB.Save(item).Error
+}
