ef3c233a2dd08e1e367023d4db5064f6ce016ce1
Brad Trammell <bradjtrammell@gmail.com>
2026-09-24 22:26:21 -0400

    feat(ui): add Groups and Audit routes

--- ui/src/layout/Layout.tsx
+++ ui/src/layout/Layout.tsx
@@ -21,6 +21,8 @@ import Login from '../user/Login';
 import Messages from '../message/Messages';
 import Settings from '../user/Settings';
 import Users from '../user/Users';
+import Groups from '../group/Groups';
+import Audit from '../audit/Audit';
 import {observer} from 'mobx-react-lite';
 import {ConnectionErrorBanner} from '../common/ConnectionErrorBanner';
 import {useStores} from '../stores';
@@ -127,6 +129,14 @@ const Layout = observer(() => {
                                     <Route
                                         path="/users"
                                         element={authed(elevated(<Users />))}
+                                    />
+                                    <Route
+                                        path="/groups"
+                                        element={authed(elevated(<Groups />))}
+                                    />
+                                    <Route
+                                        path="/audit"
+                                        element={authed(elevated(<Audit />))}
                                     />
                                     <Route
                                         path="/settings"
