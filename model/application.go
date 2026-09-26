10c9f4b0569e3a2fc3f9bbe90ce4932e17754e53
Brad Trammell <bradjtrammell@gmail.com>
2026-09-26 16:49:45 -0400

    feat: add explicit notification and chat channel types to v0.5

--- model/application.go
+++ model/application.go
@@ -2,6 +2,11 @@ package model
 
 import "time"
 
+const (
+	ChannelTypeNotification = "notification"
+	ChannelTypeChat         = "chat"
+)
+
 // Application Model
 //
 // The Application holds information about an app which can send notifications.
@@ -48,6 +53,9 @@ type Application struct {
 	//
 	// read only: true
 	AllowMemberPost bool `form:"allowMemberPost" query:"allowMemberPost" json:"allowMemberPost"`
+	// ChannelType controls how MU-aware clients present this Channel. Legacy
+	// member-posting Channels remain compatible when this value is empty.
+	ChannelType string `gorm:"type:varchar(32)" form:"channelType" query:"channelType" json:"channelType"`
 	// Whether the current requesting user receives realtime notifications from this Gotify MU channel.
 	//
 	// read only: true
