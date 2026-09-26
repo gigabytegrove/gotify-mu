bc66e43c39b58bf58b92dc1018ebadc6cf779fa8
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 21:51:36 -0400

    feat: include acknowledgement summary in messages

--- api/message.go
+++ api/message.go
@@ -780,7 +780,10 @@ func toExternalMessage(msg *model.Message) *model.MessageExternal {
 		Date:          msg.Date,
 		SenderUserID:  msg.SenderUserID,
 		SenderName:    msg.SenderName,
-		Acknowledged:  msg.Acknowledged,
+		Acknowledged:         msg.Acknowledged,
+		AcknowledgedByAnyone: msg.AcknowledgedByAnyone,
+		AcknowledgedByName:   msg.AcknowledgedByName,
+		AcknowledgedCount:    msg.AcknowledgedCount,
 	}
 	if len(msg.Extras) != 0 {
 		res.Extras = make(map[string]any)
