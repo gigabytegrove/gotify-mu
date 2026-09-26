773dca1571958c9959e1dca894190f0fe71b71ab
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 21:09:18 -0400

    fix: apply Quiet Hours Digest and Escalations to plugin messages

--- router/router.go
+++ router/router.go
@@ -133,7 +133,7 @@ func Create(db *database.GormDatabase, vInfo *model.VersionInfo, conf *config.Co
 	updateHandler := api.NewUpdateAPIFromEnv()
 	automationHandler := api.AutomationAPI{DB: db, Engine: automationEngine, WebhookLimiter: webhookLimiter}
 
-	pluginManager, err := plugin.NewManager(db, conf.PluginsDir, g.Group("/plugin/:id/custom/"), streamHandler)
+	pluginManager, err := plugin.NewManagerWithDispatcher(db, conf.PluginsDir, g.Group("/plugin/:id/custom/"), automationEngine)
 	if err != nil {
 		panic(err)
 	}
