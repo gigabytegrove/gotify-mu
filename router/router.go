d20f62e0d8376ef243d7667a5496daf491be69ad
Brad Trammell <bradjtrammell@gmail.com>
2026-09-25 21:53:47 -0400

    security: expose webhook signing-secret rotation

--- router/router.go
+++ router/router.go
@@ -320,6 +320,7 @@ func Create(db *database.GormDatabase, vInfo *model.VersionInfo, conf *config.Co
 		adminPlatform.POST("/integration/webhook", automationHandler.CreateWebhookRoute)
 		adminPlatform.PUT("/integration/webhook/:id", automationHandler.UpdateWebhookRoute)
 		adminPlatform.POST("/integration/webhook/:id/regenerate", automationHandler.RegenerateWebhookSecret)
+		adminPlatform.POST("/integration/webhook/:id/signing-secret/regenerate", automationHandler.RegenerateWebhookSigningSecret)
 		adminPlatform.DELETE("/integration/webhook/:id", automationHandler.DeleteWebhookRoute)
 
 		adminPlatform.GET("/integration/mqtt", automationHandler.GetMQTT)
