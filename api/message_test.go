685ea84881ddff95304163eec033d5bf5f1d80e9
Brad Trammell <bradjtrammell@gmail.com>
2026-09-26 12:50:26 -0400

    update v0.5 message role and collaboration tests

--- api/message_test.go
+++ api/message_test.go
@@ -62,7 +62,7 @@ func (s *MessageSuite) Test_ensureCorrectJsonRepresentation() {
 		}}},
 	}
 	test.JSONEquals(s.T(), actual, `{"paging": {"limit":5, "since": 122, "size": 5, "next": "/message?limit=5&since=122"},
-                                              "messages": [{"id":55,"appid":2,"message":"hi","title":"hi","priority":4,"date":"2017-01-02T00:00:00Z","extras":{"test::string":"string","test::array":[1,2,3],"test::int":1,"test::float":0.5}}]}`)
+                                              "messages": [{"id":55,"appid":2,"message":"hi","title":"hi","priority":4,"date":"2017-01-02T00:00:00Z","collaboration":{},"extras":{"test::string":"string","test::array":[1,2,3],"test::int":1,"test::float":0.5}}]}`)
 }
 
 func (s *MessageSuite) Test_GetMessages() {
